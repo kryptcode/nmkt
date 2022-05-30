fe2fbfe9a49afebc5536ca3b03b8f02224cb0771
kryptcode <54333700+kryptcode@users.noreply.github.com>
2022-05-30 18:03:28 +0100

    Update collections.js

--- static/collections.js
+++ static/collections.js
@@ -1,8 +1,9 @@
 import banner from '../assets/banner.png'
 import logo from '../assets/logo.png'
 
-export const doodleData = {
+export const doodlesCollection = {
   name: 'CryptoPunks',
+  creator: 'CRYP_LLC',
   description:
     'CryptoPunks launched as a fixed set of 10,000 items in mid-2017 and became one of the inspirations for the ERC-721 standard. They have been featured in places like The New York Times, Christie’s of London, Art|Basel Miami, and The PBS NewsHour.',
   isVerified: true,
@@ -16,4 +17,3 @@ export const doodleData = {
   },
   address: '0x8a90cab2b38dba80c64b7734e58ee1db38b8992e',
 }
-
